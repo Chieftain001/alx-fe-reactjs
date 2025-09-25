95d26291bcfb4dc230b8896f6038ff0b2717edf8
Samuel Gitau <samuelgitau.sg1@gmail.com>
2025-09-25 18:55:15 +0300

    Use fetchUserData in Search component for API calls

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -3,9 +3,12 @@ import Search from './components/Search';
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
+<h1 style={{ textAlign: 'center' }}>GitHub User Search</h1>
       <Search />
     </div>
   );
 }
 
 export default App;
+import Search from './components/Search';
+
