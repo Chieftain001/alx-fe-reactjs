270f6ac22af4a62edf65df0fa013f7f14698b7c2
Samuel Gitau <samuelgitau.sg1@gmail.com>
2025-09-25 18:41:34 +0300

    Use fetchUserData in Search component for API calls

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-export async function fetchUserData(username) {
-  const response = await axios.get(`https://api.github.com/users/${username}`);
-  return response.data;
-}
+// For advanced search using GitHub Search API
+export const searchUsers = async (username, location, minRepos) => {
+  let query = username ? `${username} in:login` : '';
+  if (location) query += ` location:${location}`;
+  if (minRepos) query += ` repos:>${minRepos}`;
+
+  const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
+  return response.data.items;   // returns an array of matching users
+};
