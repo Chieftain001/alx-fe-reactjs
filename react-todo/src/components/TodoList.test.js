61aaad527547fe03404376db0984be08ca06cad0
Samuel Gitau <samuelgitau.sg1@gmail.com>
2025-10-19 22:31:18 +0300

    Added TodoList.test.js to components

---
+++ react-todo/src/components/TodoList.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoList from "../components/TodoList";
+
+test("renders initial todos", () => {
+  render(<TodoList />);
+  expect(screen.getByText("Learn React")).toBeInTheDocument();
+  expect(screen.getByText("Build a Todo App")).toBeInTheDocument();
+});
+
+test("adds a new todo", () => {
+  render(<TodoList />);
+  const input = screen.getByPlaceholderText("Add todo");
+  fireEvent.change(input, { target: { value: "New Task" } });
+  fireEvent.click(screen.getByText("Add"));
+  expect(screen.getByText("New Task")).toBeInTheDocument();
+});
+
+test("toggles a todo", () => {
+  render(<TodoList />);
+  const todo = screen.getByText("Learn React");
+  fireEvent.click(todo);
+  expect(todo).toHaveStyle("text-decoration: line-through");
+});
+
+test("deletes a todo", () => {
+  render(<TodoList />);
+
+  // Delete the first todo
+  fireEvent.click(screen.getByTestId("delete-1"));
+
+  // Assert it was removed
+  expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+});
